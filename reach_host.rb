3df5bd78e48066a25383f8fe5188784197cf92ee
ibata <luc.ibata@yahoo.com>
2018-03-28 07:45:25 -0700

    Create reach_host.rb

---
+++ reach_host.rb
@@ -0,0 +1,8 @@
+control 'reach_host' do
+  impact 1.0
+  title 'This is Testing Endpoint'
+  describe command("curl -sS https://ec2us-admin-demoluc.anaplan.com/administration/about.html") do
+#    its('stdout') { should_not match /HTTP Status 404/i }
+    its('stderr') { should eq ''}
+  end
+end
