9a31d39531505047b981de36fdda4231e4b3de3c
ibata <luc.ibata@yahoo.com>
2021-02-15 14:28:49 -0800

    Update AppDelegate.m

--- react-native-netflix-master/ios/NetflixApp/AppDelegate.m
+++ react-native-netflix-master/ios/NetflixApp/AppDelegate.m
@@ -12,7 +12,7 @@
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
 
-@implementation AppDelegate
+@implementation AppDelegate-Luc
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
